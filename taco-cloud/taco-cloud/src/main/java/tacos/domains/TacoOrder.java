2180ae38faf8212b64f2893d4e41993278110dc4
Biswajeet yadav <biswajeetyadavofficial@gmail.com>
2026-06-23 03:19:56 +0530

    documenting the TacoOrder data section

--- taco-cloud/taco-cloud/src/main/java/tacos/domains/TacoOrder.java
+++ taco-cloud/taco-cloud/src/main/java/tacos/domains/TacoOrder.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
+
+// this are the details of taco.
+//and contains a arraylist which stores all the tacos order by current user.
 public class TacoOrder {
     private String deliveryName;
     private String deliveryStreet;
